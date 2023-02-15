ff2d2ae8c378c0805721d00a7a22b6adf2aa70f5
Scott Isebrand <scottisebrand@gmail.com>
2023-02-15 13:53:04 -0500

    Methods Revisited answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -56,6 +56,45 @@ for (let ninjaTurtle of ninjaTurtles) {
 
 ////////////////////////////////
 //* Methods, Revisited
+const favMovies = [
+  'Jaws',
+  'The Fellowship of the Ring',
+  "Howl's Moving Castle",
+  'Django Unchained',
+  'Cloud Atlas',
+  'The Usual Suspects',
+  'Toy Story',
+  'Conan the Barbarian',
+  'Titanic',
+  'Harry Potter',
+  'Fried Green Tomatoes',
+  'Volver',
+  'Oculus',
+  'Seven',
+  'Black Panther',
+  'Harry Potter',
+  'Imitation of Life',
+  'Snatch',
+  'Fast and Furious',
+];
+console.log(favMovies.indexOf('Titanic'));
+
+favMovies.sort();
+favMovies.pop();
+favMovies.push('Guardians of the Galaxy');
+favMovies.reverse();
+favMovies.shift();
+favMovies.unshift('The Lion in Winter');
+favMovies.indexOf('Django Unchained');
+favMovies.splice(8, 1, 'Avatar');
+const lengthOfArray = favMovies.length;
+let lastHalfOfArray = favMovies.slice(lengthOfArray / 2);
+console.log(lastHalfOfArray);
+console.log(favMovies);
+console.log(favMovies.indexOf('Fast and Furious'));
+
+// console.log(favMovies);
+
 ////////////////////////////////
 
 ////////////////////////////////
