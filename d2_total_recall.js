6dc7d2db0ae31d81b287d12fb6ce838f2b4b7c33
Scott Isebrand <scottisebrand@gmail.com>
2023-02-15 21:31:11 -0500

    Find the Median answered

--- d2_total_recall.js
+++ d2_total_recall.js
@@ -46,11 +46,11 @@
 
 ////////////////////////////////
 //* Yell at the Ninja Turtles
-const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+// const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 
-for (let ninjaTurtle of ninjaTurtles) {
-  console.log(ninjaTurtle);
-}
+// for (let ninjaTurtle of ninjaTurtles) {
+//   console.log(ninjaTurtle);
+// }
 
 ////////////////////////////////
 
@@ -77,37 +77,37 @@ const favMovies = [
   'Snatch',
   'Fast and Furious',
 ];
-console.log(favMovies.indexOf('Titanic'));
+// console.log(favMovies.indexOf('Titanic'));
 
-favMovies.sort();
-favMovies.pop();
-favMovies.push('Guardians of the Galaxy');
-favMovies.reverse();
-favMovies.shift();
-favMovies.unshift('The Lion in Winter');
-favMovies.indexOf('Django Unchained');
-favMovies.splice(8, 1, 'Avatar');
-const lengthOfArray = favMovies.length;
-let lastHalfOfArray = favMovies.slice(lengthOfArray / 2);
-console.log(lastHalfOfArray);
-console.log(favMovies);
-console.log(favMovies.indexOf('Fast and Furious'));
+// favMovies.sort();
+// favMovies.pop();
+// favMovies.push('Guardians of the Galaxy');
+// favMovies.reverse();
+// favMovies.shift();
+// favMovies.unshift('The Lion in Winter');
+// favMovies.indexOf('Django Unchained');
+// favMovies.splice(8, 1, 'Avatar');
+// const lengthOfArray = favMovies.length;
+// let lastHalfOfArray = favMovies.slice(lengthOfArray / 2);
+// console.log(lastHalfOfArray);
+// console.log(favMovies);
+// console.log(favMovies.indexOf('Fast and Furious'));
 
 ////////////////////////////////
 
 ////////////////////////////////
 //* Where is Waldo
-const whereIsWaldo = [
-  ['Timmy', 'Frank'],
-  'Eggbert',
-  ['Lucinda', 'Jacc', 'Neff', 'Snoop'],
-  ['Petunia', ['Baked Goods', 'Waldo']],
-];
+// const whereIsWaldo = [
+//   ['Timmy', 'Frank'],
+//   'Eggbert',
+//   ['Lucinda', 'Jacc', 'Neff', 'Snoop'],
+//   ['Petunia', ['Baked Goods', 'Waldo']],
+// ];
 
-console.log(whereIsWaldo.splice([1], 1));
-whereIsWaldo[1][2] = 'No one';
-console.log(whereIsWaldo);
-console.log(whereIsWaldo[2][1][1]);
+// console.log(whereIsWaldo.splice([1], 1));
+// whereIsWaldo[1][2] = 'No one';
+// console.log(whereIsWaldo);
+// console.log(whereIsWaldo[2][1][1]);
 
 // let whereIsWaldoSlice = whereIsWaldo.slice()
 // console.log(whereIsWaldo);
@@ -116,24 +116,34 @@ console.log(whereIsWaldo[2][1][1]);
 
 ////////////////////////////////
 //*  Excited Kitten (using https://speaklolcat.com/)
-
-excitedKittenString = 'Love me, pet me! HSSSSSS!';
-for (i = 1; i <= 20; i++) {
-  if (i % 2 === 0) {
-    let kittyTalk = [
-      'HOOMAN, Y R U TAKIN PICTUREZ OV ME?',
-      'TEH KATNIP MADE ME DO IT',
-      'Y DOEZ TEH RED DOT ALWAYS GIT AWAY.',
-    ];
-    let randomNum = Math.floor(Math.random() * 3);
-    console.log(kittyTalk[randomNum]);
-  } else {
-    console.log(i, excitedKittenString);
-  }
-}
-
-////////////////////////////////
+// excitedKittenString = 'Love me, pet me! HSSSSSS!';
+// for (i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     let kittyTalk = [
+//       'HOOMAN, Y R U TAKIN PICTUREZ OV ME?',
+//       'TEH KATNIP MADE ME DO IT',
+//       'Y DOEZ TEH RED DOT ALWAYS GIT AWAY.',
+//     ];
+//     let randomNum = Math.floor(Math.random() * 3);
+//     console.log(i, kittyTalk[randomNum]);
+//   } else {
+//     console.log(i, excitedKittenString);
+//   }
+// }
 
 ////////////////////////////////
-//*  Find the Median
+
+////////////////////////////////
+//*  Find the Median.... number in the following "nums" array, then console.log that number. hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into "Math.floor( // something )"
+const nums = [
+  14, 11, 16, 15, 13, 16, 15, 17, 19, 11, 12, 14, 19, 11, 15, 17, 11, 18, 12,
+  17, 12, 71, 18, 15, 12,
+];
+nums.sort();
+// console.log(nums);
+let numsLength = nums.length;
+let numsLengthDividedByTwo = numsLength / 2;
+let numsMedianIndex = Math.floor(numsLengthDividedByTwo);
+console.log(nums[numsMedianIndex]);
+
 ////////////////////////////////
