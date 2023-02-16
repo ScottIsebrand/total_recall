25cc445b8d48566927fa48dfc5db1dac85769394
Scott Isebrand <scottisebrand@gmail.com>
2023-02-16 09:58:36 -0500

    created hamster class

---
+++ d3_total_recall.js
@@ -0,0 +1,17 @@
+// SECTION 1 Creating Classes & Factories
+class Hamster {
+  constructor(name) {
+    this.owner = '';
+    this.name = name;
+    this.price = 15;
+  }
+  wheelRun() {
+    console.log('squeak squeak');
+  }
+  eatFood() {
+    console.log('nibble nibble');
+  }
+  getPrice() {
+    return this.price;
+  }
+}
